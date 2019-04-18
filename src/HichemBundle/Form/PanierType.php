842026b2842babd3df155fbde939377706c5b1d0
Hichemgr <47151207+Hichemgr@users.noreply.github.com>
2019-04-18 23:52:21 +0200

    commande

---
+++ src/HichemBundle/Form/PanierType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HichemBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PanierType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('Idpanier')->add('etat')->add('Ajouter',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HichemBundle\Entity\Panier'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'hichembundle_panier';
+    }
+
+
+}
